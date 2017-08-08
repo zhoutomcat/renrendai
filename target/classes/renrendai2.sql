d57e84a94db8e2681118f0c52153c29fa98c0f61
zhoutomcat <987758377@qq.com>
2017-08-08 14:12:42 +0800

    数据库表已基本建好   静态页面导入完成

--- target/classes/renrendai2.sql
+++ target/classes/renrendai2.sql
@@ -8,10 +8,8 @@ create table User(
       u_creditnumber  		int default(0) not null,  --信誉积分
       u_creditdegree   	varchar(6) default('HR') check in('AA','A','B','C','D','HR'), --信誉等级
       u_registerdate 		date   --注册时间
-      
-      u_tel varchar(11),			--手机号码							新增
-	  u_email varchar(50)			--邮箱							新增
-      
+      u_tel                     varchar(11),			--手机号码							新增
+	  u_email                varchar(50),			--邮箱							新增
 	  temp varchar(100) default null,
 	  temp varchar(100) default null,
 	  temp varchar(100) default null
@@ -30,7 +28,6 @@ create table UserFund(
 	   
 )
 
-
 --借贷表
 udi_moneysure           number(10,2), --已借金额  		--去掉 		 修改	--把这个字段放在用户表中（可以统计每个用户每次借款的信息）
 create table UserDebitIn(
@@ -46,6 +43,7 @@ create table UserDebitIn(
        udi_refundrealitydate	date, --实际还贷日期        --影响信额度 
        udi_use	                varchar(2000)	      --借款用途
        udi_refundway		int 	--还款方式（月还贷 1，一次性还贷 0）
+       udi_type          varchar(100),      --借贷类型          暂定              以后用的时候再说
 	   temp varchar(100) default null,
 	   temp varchar(100) default null,
 	   temp varchar(100) default null	   
@@ -58,7 +56,8 @@ create table UserDebitOut(
        u_id 		int  --user的外键(放贷人id)  一对多
        udo_startdate		date, --放贷开始的时间()
        udo_date 		date, --放贷贷日期(放贷成功时)   
-       udo_money 		number(10,2)	--放贷的金额	
+       udo_money 		number(10,2)	--放贷的金额
+       udo_type     varchar(100),            --放贷类型          暂定              以后用的时候再说 
 	   temp varchar(100) default null,
 	   temp varchar(100) default null,
 	   temp varchar(100) default null       
@@ -77,17 +76,7 @@ create table PerRefund(
 	temp varchar(100) default null
 )
 
---用户日志：
-create table UserLogger(
-	ul_id 			int primary key auto_increment ,   --主键
-	u_id 		        int --  用户id
-	ul_optype	        int ,                 --操作类型（。。。）
-	ul_money 		number(10,2),         --操作金额
-	ul_time 		date                --时间
-	temp varchar(100) default null,
-	temp varchar(100) default null,
-	temp varchar(100) default null
-)
+
 
 
 --关注投标表: 
@@ -96,7 +85,8 @@ create table AttentionMark(
 	u_id  			int,   --关注人的id
 	udi_id  		int, --借贷表id外键
 	am_time 		date,   --关注时间
-	am_stauts	int 	--关注状态   0代表取消关注   1代表正在关注			修改
+	am_stauts	int, 	--关注状态   0代表取消关注   1代表正在关注			修改
+	am_weight   int  default 0,        --权重越大     越排在前面
 	temp varchar(100) default null,
 	temp varchar(100) default null,
 	temp varchar(100) default null
@@ -115,15 +105,6 @@ create table AttentionUser(
 	temp varchar(100) default null	
 )
 
---后台系统日志:   
-create table AdminLogger(
-	al_id 			int primary key auto_increment ,   --主键
-	a_id 			int   --           管理员id
-	al_content 		varchar(2000),    --内容(xxx用户，进行了xxx操作，） 
-	al_optype 		int ,                 --操作类型（。。。）
-	al_opclassname 	varchar(100),       --操作的表名
-	al_time 		date                --时间
-)
 --管理员表
 create table Admin(
 	a_id 			int primary key auto_increment,
@@ -139,37 +120,27 @@ create table Admin(
 --消息
 create table AdminToUserMessage(
 	atum_id 			int primary key auto_increment,
-	atum_id 			int ,                   --用户id
+	u_id 			int ,                   --用户id
 	atum_time 			date,               --时间
 	atum_content 		varchar(2000),   --内容
 	atum_status 		int default 1   --  0 表示已读  1  表示未读
 )
 
-
-
 --个人资料
 create table UserMessage(
        um_id   		int primary key auto_increment,	   --
+       u_id          int,        --用户表id
        um_reallyName 	varchar(100),	   --真实姓名
-       um_idCard     	varchar(100),	   --省份证
+       um_idCard     	varchar(100),	   --身份证
        um_tel  	     	varchar(100),	   --手机号码
-       um_sex       	 int,	   --性别 1 男  0 女      
-       um_nowPlaceTel   varchar(100),   --居住地电话
-       um_image           --图像
+       um_sex       	 int,	         --性别    年龄     出生日期    居住地    都通过身份证获取
+       um_age         int,
+       um_birthday  date,                  --出生日期 
+       um_nowPlace varchar(200),   --居住地
+       um_email varchar(100),
+       um_image           --图像                          --资料就先写这么多，像人人贷一样,到时候可以接一些接口,个人征信报告，微粒贷记录
        
 );
-
-create table Dictionary()
-
-
-
-
-
-
----注意每项都加两个冗余
-  分别以类每个单词首字母+1/2
-   如: User   u1 和  u2
-UserMessage  um1 和  um2
 
 
 
@@ -183,5 +154,42 @@ create table bankInfo(
 )
 
 
+
+
+
+
+
+
+---注意每项都加两个冗余
+  分别以类每个单词首字母+1/2
+   如: User   u1 和  u2
+UserMessage  um1 和  um2
+
+
+--后台系统日志:   
+create table AdminLogger(
+	al_id 			int primary key auto_increment ,   --主键
+	a_id 			int   --           管理员id
+	al_content 		varchar(2000),    --内容(xxx用户，进行了xxx操作，） 
+	al_optype 		int ,                 --操作类型（。。。）
+	al_opclassname 	varchar(100),       --操作的表名
+	al_time 		date                --时间
+)
+
+--用户日志：
+create table UserLogger(
+	ul_id 			int primary key auto_increment ,   --主键
+	u_id 		        int --  用户id
+	ul_optype	        int ,                 --操作类型（。。。）
+	ul_money 		number(10,2),         --操作金额
+	ul_time 		date                --时间
+	temp varchar(100) default null,
+	temp varchar(100) default null,
+	temp varchar(100) default null
+)
+
+
+
+create table Dictionary()
 
 
--- target/classes/renrendaidata.sql
+++ target/classes/renrendaidata.sql
@@ -11,6 +11,115 @@ insert into user(u_name,u_password,u_creditnumber,u_creditdegree,u_registerdate)
 insert into user(u_name,u_password,u_creditnumber,u_creditdegree,u_registerdate) values('b','b','','',to_date('2017/7/26','yyyy/MM/dd'));
 insert into user(u_name,u_password,u_creditnumber,u_creditdegree,u_registerdate) values('c','c','','',to_date('2017/7/27','yyyy/MM/dd'));
 insert into user(u_name,u_password,u_creditnumber,u_creditdegree,u_registerdate) values('d','d','','',to_date('2017/7/28','yyyy/MM/dd'));
+<<<<<<< HEAD
+insert into user(u_name,u_password,u_creditnumber,u_creditdegree,u_registerdate) values('e','e','','',to_date('2017/7/28','yyyy/MM/dd'));
+insert into user(u_name,u_password,u_creditnumber,u_creditdegree,u_registerdate) values('f','f','','',to_date('2017/7/28','yyyy/MM/dd'));
+insert into user(u_name,u_password,u_creditnumber,u_creditdegree,u_registerdate) values('g','g','','',to_date('2017/7/28','yyyy/MM/dd'));
+select * from user;
+
+
+----------------------------------------
+insert into userFund(u_id,uf_balance,uf_available,uf_freeze) values(1,0.00,0.00,0.00);
+insert into userFund(u_id,uf_balance,uf_available,uf_freeze) values(2,0.00,0.00,0.00);
+insert into userFund(u_id,uf_balance,uf_available,uf_freeze) values(3,0.00,0.00,0.00);
+select * from userFund;
+commit;
+
+------------------------------------------
+insert into userDebitIn(udi_title,u_idin,udi_money,udi_status,udi_profit, udi_publishdate, udi_date,
+udi_refundnormaldate,udi_refundrealitydate,udi_use,udi_refundway)
+ values('买房',1,100000,1,12.50,
+to_date('2013/7/28','yyyy/MM/dd'),
+to_date('2013/7/28','yyyy/MM/dd'),
+to_date('2013/7/28','yyyy/MM/dd'),
+to_date('2013/7/28','yyyy/MM/dd'),
+'计划在海边买一栋别墅',1);
+insert into userDebitIn(udi_title,u_idin,udi_money,udi_status,udi_profit, udi_publishdate, udi_date,
+udi_refundnormaldate,udi_refundrealitydate,udi_use,udi_refundway)
+ values('买车',2,100000,1,12.50,
+to_date('2013/7/28','yyyy/MM/dd'),
+to_date('2013/7/28','yyyy/MM/dd'),
+to_date('2013/7/28','yyyy/MM/dd'),
+to_date('2013/7/28','yyyy/MM/dd'),
+'计划在海边买一栋别墅',1);
+insert into userDebitIn(udi_title,u_idin,udi_money,udi_status,udi_profit, udi_publishdate, udi_date,
+udi_refundnormaldate,udi_refundrealitydate,udi_use,udi_refundway)
+ values('买药',3,100000,1,12.50,
+to_date('2013/7/28','yyyy/MM/dd'),
+to_date('2013/7/28','yyyy/MM/dd'),
+to_date('2013/7/28','yyyy/MM/dd'),
+to_date('2013/7/28','yyyy/MM/dd'),
+'计划在海边买一栋别墅',1);
+insert into userDebitIn(udi_title,u_idin,udi_money,udi_status,udi_profit, udi_publishdate, udi_date,
+udi_refundnormaldate,udi_refundrealitydate,udi_use,udi_refundway)
+ values('投资',4,100000,1,12.50,
+to_date('2013/7/28','yyyy/MM/dd'),
+to_date('2013/7/28','yyyy/MM/dd'),
+to_date('2013/7/28','yyyy/MM/dd'),
+to_date('2013/7/28','yyyy/MM/dd'),
+'计划在海边买一栋别墅',0);
+select * from userDebitIn;     --uid唯一       只能借贷一次，除非已经还款
+-----------------------------------
+insert into userDebitOut(udi_id,u_id,udo_startdate,udo_date,udo_money) values(1,5,to_date('2013/7/28','yyyy/MM/dd'),to_date('2013/7/28','yyyy/MM/dd'),10000);
+insert into userDebitOut(udi_id,u_id,udo_startdate,udo_date,udo_money) values(1,6,to_date('2013/7/28','yyyy/MM/dd'),to_date('2013/7/28','yyyy/MM/dd'),10000);
+insert into userDebitOut(udi_id,u_id,udo_startdate,udo_date,udo_money) values(1,7,to_date('2013/7/28','yyyy/MM/dd'),to_date('2013/7/28','yyyy/MM/dd'),10000);
+insert into userDebitOut(udi_id,u_id,udo_startdate,udo_date,udo_money) values(2,8,to_date('2013/7/28','yyyy/MM/dd'),to_date('2013/7/28','yyyy/MM/dd'),10000);
+insert into userDebitOut(udi_id,u_id,udo_startdate,udo_date,udo_money) values(2,9,to_date('2013/7/28','yyyy/MM/dd'),to_date('2013/7/28','yyyy/MM/dd'),10000);
+insert into userDebitOut(udi_id,u_id,udo_startdate,udo_date,udo_money) values(2,7,to_date('2013/7/28','yyyy/MM/dd'),to_date('2013/7/28','yyyy/MM/dd'),10000);
+------------------------------------
+--自动生成
+--insert into r_PerRefund values(1,to_date('2013/8/28','yyyy/MM/dd'),10000,0,1)
+-------------------------------------
+insert into AttentionMark(u_id,udi_id,am_time,am_status,am_weight) values(6,1,to_date('2013/7/28','yyyy/MM/dd'),1,default);
+insert into AttentionMark(u_id,udi_id,am_time,am_status,am_weight) values(7,1,to_date('2013/7/28','yyyy/MM/dd'),1,10);
+insert into AttentionMark(u_id,udi_id,am_time,am_status,am_weight) values(8,1,to_date('2013/7/28','yyyy/MM/dd'),1,default);
+insert into AttentionMark(u_id,udi_id,am_time,am_status,am_weight) values(9,2,to_date('2013/7/28','yyyy/MM/dd'),1,default);
+select * from AttentionMark;
+--------------------------------------------------------------------
+insert into AttentionUser(u_id,u_idbyAttention,au_time,au_status) values(1,5,to_date('2013/7/28','yyyy/MM/dd'),1);
+insert into AttentionUser(u_id,u_idbyAttention,au_time,au_status) values(2,6,to_date('2013/7/28','yyyy/MM/dd'),1);
+insert into AttentionUser(u_id,u_idbyAttention,au_time,au_status) values(3,6,to_date('2013/7/28','yyyy/MM/dd'),1);
+insert into AttentionUser(u_id,u_idbyAttention,au_time,au_status) values(4,6,to_date('2013/7/28','yyyy/MM/dd'),0);
+select * from AttentionUser;
+--------------------------------------------------------------------
+insert into Admins values(1,'','','管理员a','a',1,1);
+insert into Admins values(2,'','','管理员b','b',0,1);
+-------------------------------------------------------------------
+insert into userLogger values(1,'','','1关注了用户b',0,1,to_date('2013/7/28','yyyy/MM/dd'),1,null,null,null);
+insert into userLogger values(2,'','','1关注了用户b',0,1,to_date('2013/7/28','yyyy/MM/dd'),1,null,null,null);
+insert into userLogger values(3,'','','1关注了用户b',0,1,to_date('2013/7/28','yyyy/MM/dd'),1,null,null,null);
+select * from AdminLogger;
+ UL_ID
+ UL1
+ UL2
+ UL_CONTE
+ UL_MONEY
+ UL_OP_TY
+ UL_TIME
+ U_ID
+ UBY_ID
+ UDI_ID
+ UDO_ID
+--------------------------------------------------------------------
+commit;
+insert into AdminToUserMessage values(1,'','','恭喜您在注册成功',1,to_date('2013/7/28','yyyy/MM/dd'),1);
+--------------------------------------------------------------------
+--insert into userlogger values(1,'','','a说你的信用有问题',0,1,to_date('2013/7/28','yyyy/MM/dd'),1,1,2,2);
+select * from userlogger;
+----------------------------------------------------------------------
+insert into  admin(a_name,a_password,a_role,a_status) values('yc','a',default,default);
+
+----------------------------------------------------------------------
+insert into AdminToUserMessage(u_id,atum_time,atum_content,atum_status) values(1,to_date('2013/7/28','yyyy/MM/dd'),'请的借款期限快到了，请及时还款',1);
+insert into AdminToUserMessage(u_id,atum_time,atum_content,atum_status) values(2,to_date('2013/7/28','yyyy/MM/dd'),'请的借款期限快到了，请及时还款',0);
+insert into AdminToUserMessage(u_id,atum_time,atum_content,atum_status) values(3,to_date('2013/7/28','yyyy/MM/dd'),'请的借款期限快到了，请及时还款',1);
+insert into AdminToUserMessage(u_id,atum_time,atum_content,atum_status) values(4,to_date('2013/7/28','yyyy/MM/dd'),'请的借款期限快到了，请及时还款',0);
+
+------------------------------------------------------------------------
+insert into UserMessage(u_id,) values(2,'','','15386028352','404html.jpg','衡阳','蔡蒂民',1,'13812345678',2);
+insert into UserMessage values(3,'','','17674756360','404html.jpg','衡阳','蔡蒂民',1,'13812345678',3);
+------------------------------------------------------------------------
+=======
 select * from user;
 
 
@@ -98,6 +207,7 @@ select * from userMessage;
 --insert into userlogger values(1,'','','a说你的信用有问题',0,1,to_date('2013/7/28','yyyy/MM/dd'),1,1,2,2);
 select * from userlogger;
 ----------------------------------------------------------------------
+>>>>>>> refs/remotes/origin/zzc
 insert into Dictionary values(1,1,'男','sex');
 insert into Dictionary values(2,0,'女','sex');
 insert into Dictionary values(3,1,'显示','status');
